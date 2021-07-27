0ae3197f9eed022f9ed58c54671c75337a48d9a8
Moutinho <alexandre.moutinho@live.fr>
2021-07-27 16:47:04 +0200

    :wrench: add connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const app = require('./app')
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.sp2o1.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority)`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.on("error", err => {
+    console.log("Mongoose connection error: " + err);
+})
+
+mongoose.connection.once("open", () => {
+    console.log("MongoDB connected");
+})
+
+const app = require('./app');
 
 app.listen(8000, () => {
     console.log("Server listening on port 8000")
