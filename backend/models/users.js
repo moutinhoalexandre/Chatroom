445d25bcbc0ae3facb6278a94567b556cf4b7732
Moutinho <alexandre.moutinho@live.fr>
2021-07-27 16:59:02 +0200

    :wrench: add mongoose-unique-validator

---
+++ backend/models/users.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator'); //Allows us to check that the field with the unique property is not already present in the database
+
