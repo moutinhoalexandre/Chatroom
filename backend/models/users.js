99cce930935f16528bb220c3b73450cfc1ddeea3
Moutinho <alexandre.moutinho@live.fr>
2021-08-02 11:37:11 +0200

    :wrench: add users.js models

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,3 +1,32 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator'); //Allows us to check that the field with the unique property is not already present in the database
 
+//We create a data schema using mongoose with the desired properties
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            unique: true,
+            required: "Name is required",
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: "Email is required",
+        },
+        password: {
+            type: String,
+            required: "Password is required",
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+//and apply unique-validator to the schema
+userSchema.plugin(uniqueValidator);
+
+// Then export this schema as a model.
+module.exports = mongoose.Schema("Users", userSchema);
+
