d26e51df229ed0eb59d30250fc11b6261a29b01b
Moutinho <alexandre.moutinho@live.fr>
2021-08-02 11:42:19 +0200

    :wrench:  add chatroom.js models

---
+++ backend/models/chatroom.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator"); //Allows us to check that the field with the unique property is not already present in the database
+
+//We create a data schema using mongoose with the desired properties
+const chatroomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: "Name is required",
+  },
+});
+
+//and apply unique-validator to the schema
+userSchema.plugin(uniqueValidator);
+
+// Then export this schema as a model.
+module.exports = mongoose.Schema("Chatroom", chatroomSchema);
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator'); //Allows us to check that the field with the unique property is not already present in the database
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator"); //Allows us to check that the field with the unique property is not already present in the database
 
 //We create a data schema using mongoose with the desired properties
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            unique: true,
-            required: "Name is required",
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: "Email is required",
-        },
-        password: {
-            type: String,
-            required: "Password is required",
-        }
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: "Name is required",
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is required",
+    },
+    password: {
+      type: String,
+      required: "Password is required",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 //and apply unique-validator to the schema
@@ -29,4 +29,3 @@ userSchema.plugin(uniqueValidator);
 
 // Then export this schema as a model.
 module.exports = mongoose.Schema("Users", userSchema);
-
